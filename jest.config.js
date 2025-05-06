7d70bdd9b74bd2a888bb4393f6d4570530dfdeb8
highlyprofitable108 <mikefusc@me.com>
2025-05-06 17:27:08 -0400

    feat: configure jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$       ': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$       ': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }],
+  },
+}; 
